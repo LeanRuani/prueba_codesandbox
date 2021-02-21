d2d7e1404e8d2326a98dcaef5eea9e0e815ac01b
Leandro <46205092+LeanRuani@users.noreply.github.com>
2021-02-21 11:55:50 -0300

    title cambio en el title

--- src/App.js
+++ src/App.js
@@ -3,8 +3,8 @@ import "./styles.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      <h1>Hello mundo</h1>
+      <h2>Esto es una prueba de sandbox</h2>
     </div>
   );
 }
